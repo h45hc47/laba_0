6973a9151269fd6a5babb1c6fd71cfb5e7374fd7
h45hc47 <h45hc47@autistici.org>
2021-09-29 13:14:25 +0300

    Diff between the second and first nums is replaced by the square root of the sum of all three numbers

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 
 
 #include <stdio.h>
+#include <math.h>
 
 
-void get_numbers(const char* msg, int nums[3]) {
+void get_numbers(const char* msg, float nums[3]) {
     char answer[256];
 
     printf("%s", msg);
     fgets(answer, sizeof(answer), stdin);
 
-    while (sscanf(answer, "%d %d %d", &nums[0], &nums[1], &nums[2]) != 3) {
+    while (sscanf(answer, "%f %f %f", &nums[0], &nums[1], &nums[2]) != 3) {
         printf("Incorrect input. Try again: ");
         fgets(answer, sizeof(answer), stdin);
     }
@@ -19,11 +20,14 @@ void get_numbers(const char* msg, int nums[3]) {
 
 int main(void) {
     const char* msg = "Enter three numbers: ";
-    int nums[3];
+    float nums[3];
+    float sum;
 
     get_numbers(msg, nums);
 
-    printf("Second number - first number = %d", nums[1] - nums[0]);
+    sum = nums[0] + nums[1] + nums[2];
+
+    printf("Square root of sum of three numbers = %f", sqrt(sum));
 
     return 0;
 }
